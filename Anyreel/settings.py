1cfc7a5e300fcb4924996e6ea7c8c76a257d1fd8
Emir Borovac <142483523+Emirborovac@users.noreply.github.com>
2024-09-02 21:04:05 +0300

    Static fix 2

--- Anyreel/settings.py
+++ Anyreel/settings.py
@@ -127,7 +127,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 import os
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')]
 # settings.py
 
 import os
